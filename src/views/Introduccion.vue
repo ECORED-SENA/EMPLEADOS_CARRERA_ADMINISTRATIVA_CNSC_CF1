cee7030328bd5c34c89fb7203398aae53d1a0fc1
Luis Perez <ljpm9401@gmail.com>
2021-06-28 08:16:27 -0500

    actualizacion video introduccion

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,7 +13,7 @@
     
     figure.mt-4
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/FAhJrdAT-VE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     p.mt-4 La normatividad determina los criterios y directrices en el proceso de evaluación del desempeño laboral (EDL) y las empresas del Estado que no cuenten con un sistema de evaluación propio deben adoptar el sistema tipo de la Comisión Nacional del Servicio Civil (CNSC), definido mediante el Acuerdo 6176 de 2018 y su Anexo Técnico, en el que se definen los medios o instrumentos para llevar a cabo el proceso de evaluación del desempeño laboral. El Acuerdo rige para todas las entidades a partir del 1° de febrero de 2019.
       
